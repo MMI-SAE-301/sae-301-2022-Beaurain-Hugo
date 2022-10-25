965bb9bd175273360f8cc8342ee9b000d90970f3
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-25 16:35:47 +0200

    Import de ref de vue et ajout de la const montre

--- src/components/formMontre.vue
+++ src/components/formMontre.vue
@@ -2,9 +2,12 @@
     import montreCarre from '@/components/montreCarreeSvg.vue'
     import montreRonde from '@/components/montreRondeSvg.vue'
     import type { Montre } from '@/types'
+    import { ref } from 'vue'
 
     const props = defineProps<{
         data?: Montre;
         id?: string;
     }>();
+    const montre = ref<Montre>(props.data ?? {});
+
 </script>
