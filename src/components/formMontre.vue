f3f534d639e6e0c50553b53fd42ac062b352fa5d
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-25 16:21:33 +0200

    Import du type montre du types.ts

--- src/components/formMontre.vue
+++ src/components/formMontre.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
     import montreCarre from '@/components/montreCarreeSvg.vue'
     import montreRonde from '@/components/montreRondeSvg.vue'
+    import type { Montre } from '@/types'
 </script>
