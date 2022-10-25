03c3a84b615d0dce136039f37f1c1070b0962691
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-25 15:51:26 +0200

    Ajout du router-link menant à l'index.vue de montres dans app.vue

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,12 @@
           <code class="font-mono">/src/pages/index.vue</code>
         </router-link>
       </li>
+      <li>
+        <router-link class="text-red-600 underline" to="/montres">
+          lien vers
+          <code class="font-mono">/src/pages/montres/index.vue</code>
+        </router-link>
+      </li>
     </ul>
   </nav>
 
