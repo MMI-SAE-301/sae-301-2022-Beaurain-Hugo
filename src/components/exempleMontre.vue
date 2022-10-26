8110c13f7c3a069d2510b3823bab5f89c89c246f
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-26 13:03:29 +0200

    Import et apparition de toutes les montres de supabase

---
+++ src/components/exempleMontre.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    import { supabase } from "@/supabase";
+    import montreCarree from "@/components/montreCarreeSvg.vue";
+    const props = defineProps<{
+      max?: number;
+    }>();
+    const { data: montres, error } = await supabase
+      .from("alluser")
+      .select("*")
+      .limit(props.max ?? 100);
+        if (error) {
+          console.log("n'a pas pu récupérer les montres", { error });
+        }
+</script>
+<template>
+    <ul>
+      <li v-for="montre in montres" :key="montre.id">
+        <!--<router-link
+          :to="{ name: 'montres-edit-id', params: { id: montre.id } }"
+        >-->
+          <montreCarree class="w-64" v-bind="montre" />
+          <!--<router-link :to="{ name:'basket-delete-delete', params: {id:basket.id}}">Supprimer</router-link>
+        </router-link>-->
+      </li>
+    </ul>
+  </template>
