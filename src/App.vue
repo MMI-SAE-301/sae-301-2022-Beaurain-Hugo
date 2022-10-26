f1027830c74e506717c022e5d7827758cde18171
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-26 14:16:05 +0200

    Début header mobile : nom site et icone menu hamburger

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <nav>
-    <h4 class="text-xl">
-      <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
-      menu (dans <code class="font-mono">/src/App.vue</code>)
-    </h4>
-    <ul>
-      <li>
-        <router-link class="text-red-600 underline" to="/">
-          lien vers
-          <code class="font-mono">/src/pages/index.vue</code>
-        </router-link>
-      </li>
-      <li>
-        <router-link class="text-red-600 underline" to="/montres">
-          lien vers
-          <code class="font-mono">/src/pages/montres/index.vue</code>
-        </router-link>
-      </li>
-      <li>
-        <router-link class="text-red-600 underline" to="/montres/new">
-          lien vers
-          <code class="font-mono">/src/pages/montres/new.vue</code>
-        </router-link>
-      </li>
-      <li>
-        <router-link class="text-red-600 underline" to="/connexion">
-          lien vers
-          <code class="font-mono">/src/pages/connexion.vue</code>
-        </router-link>
-      </li>
-    </ul>
-  </nav>
+    <header>
+    <nav>
+      <div class="flex justify-between">
+      <router-link to="/" class="font-cinzel text-lg">Tik Tak</router-link>        
+      <button>
+        <Bars3Icon class="h-10 w-10" />
+        <span class="sr-only">Menu</span>
+      </button>
+    </div>
+      <ul>
+        <li>
+          <router-link class="text-red-600 underline" to="/">
+            lien vers
+            <code class="font-mono">/src/pages/index.vue</code>
+          </router-link>
+        </li>
+        <li>
+          <router-link class="text-red-600 underline" to="/montres">
+            lien vers
+            <code class="font-mono">/src/pages/montres/index.vue</code>
+          </router-link>
+        </li>
+        <li>
+          <router-link class="text-red-600 underline" to="/montres/new">
+            lien vers
+            <code class="font-mono">/src/pages/montres/new.vue</code>
+          </router-link>
+        </li>
+        <li>
+          <router-link class="text-red-600 underline" to="/connexion">
+            lien vers
+            <code class="font-mono">/src/pages/connexion.vue</code>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 
   <!-- Affiche les pages -->
   <Suspense>
