ef1689f4286c6228abc0d6894e08f30da60f4d77
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-26 10:27:40 +0200

    Ajout de la réception d'id quand dans la page edit

---
+++ src/pages/montres/edit/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import formMontre from '@/components/formMontre.vue'
+    defineProps<{
+        id: string;
+    }>();
+</script>
+
+<template>
+    <formMontre :id="id" />
+</template>
