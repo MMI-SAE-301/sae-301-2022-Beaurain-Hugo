56a218fe1ffd9184d6827251ac757508405dabcc
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-26 09:13:44 +0200

    Ajout du bouton de déconnexion à la page connexion et apparition des boutons de connexion seulement si l'utilisateur n'est pas connecté

--- src/pages/connexion.vue
+++ src/pages/connexion.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
     import logInLogOutGoogle from '@/components/logInLogOutGoogle.vue'
     import logInLogOutFacebook from '@/components/logInLogOutFacebook.vue'
+    import {supabase, user } from '../supabase'
+
 </script>
 
 <template>
-    <logInLogOutGoogle />
-    <logInLogOutFacebook />
+    <button v-if="user" @pointerdown="supabase.auth.signOut()">
+        Se déconnecter ({{user.email}})
+    </button>
+    <div v-else>
+        <logInLogOutGoogle />
+        <logInLogOutFacebook />
+    </div>
 </template>
 
