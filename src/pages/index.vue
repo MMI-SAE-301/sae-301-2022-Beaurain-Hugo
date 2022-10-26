d4c69bf8e49d1aed416f9a24aebd88f819e3b6f0
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-26 13:03:49 +0200

    Import de la liste de toutes les montres et affichage de seulement 3

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+  import exempleMontre from '@/components/exempleMontre.vue'
+</script>
+
 <template>
   <section>
     <h1 class="text-2xl">
       Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
     </h1>
   </section>
+  <section>
+  <h2>
+    Exemple de montres
+  </h2>
+  <exempleMontre class="flex flex-wrap gap-2" :max="3" />
+</section>
 </template>
