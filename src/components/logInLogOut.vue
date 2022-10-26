7329e0e43730e0c3bf8045018a2d12f8a04d0575
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-26 08:58:02 +0200

    Ajout du code pour connexion avec google

--- src/components/logInLogOut.vue
+++ src/components/logInLogOut.vue
@@ -1,3 +1,12 @@
 <script setup lang="ts">
     import {supabase, user } from '../supabase'
 </script>
+
+<template>
+    <button v-if="user" @pointerdown="supabase.auth.signOut()">
+        Se déconnecter ({{user.email}})
+    </button>
+    <button v-else @pointerdown="supabase.auth.signIn({provider: 'google'})">
+        Se connecter avec Google
+    </button>
+</template>
