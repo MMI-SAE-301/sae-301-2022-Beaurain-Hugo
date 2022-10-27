09c0a39af00c81143fbbceeaaea60a768a7c6d94
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-27 13:13:40 +0200

    Import et affichage des matériaux dans le formulaire

--- src/components/formMontre.vue
+++ src/components/formMontre.vue
@@ -2,6 +2,8 @@
     import montreCarree from '@/components/montreCarreeSvg.vue'
     import montreRonde from '@/components/montreRondeSvg.vue'
     import listColors from '@/components/listColors.vue'
+    import listMateriauxBra from '@/components/listMateriauxBra.vue'
+    import listMateriauxBoi from '@/components/listMateriauxBoi.vue'
     import type { Montre } from '@/types'
     import { ref } from 'vue'
     import { useRouter } from 'vue-router'
@@ -33,12 +35,17 @@
 
 <template>
     
-    <div class="grid sm:grid-cols-2">
+    <div class="grid sm:grid-cols-2 mb-10">
         <FormKit submit-label="Enregistrer" :submit-attrs="{  classes: { input: 'ml-40 font-lexend text-base mt-16 sm:text-lg rounded-sm py-3 px-6 sm:py-4 sm:px-8 bg-blue-300 flex' } }" type="form" v-model="montre" @submit="upsertBasket">
-        <p class="font-lexend text-xl">Couleurs</p>
+        <p class="font-lexend text-xl mt-10">Couleurs</p>
         <listColors name="bracelet" label="Bracelet" />
         <listColors name="boitier" label="Boîtier" />
         <listColors name="ecran" label="Ecran" />
+        <p class="font-lexend text-xl mt-5">Matériaux</p>
+        <listMateriauxBoi label="Boîtier" />
+        <listMateriauxBra label="Bracelet" />
+        <FormKit name="commande" label="Commander ?" type="checkbox" wrapper-class="w-full flex text-xl"/>            
+
     </FormKit>
     <div>
         <montreCarree class="w-64 my-4 mx-auto" v-bind="montre"/>
