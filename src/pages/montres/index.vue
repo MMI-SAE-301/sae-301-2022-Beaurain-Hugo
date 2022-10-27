eac5fc0149c6d9ff7ec34d0069014e7127651ff4
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-27 09:44:04 +0200

    Ajout d'une div avec marge autour des montres de l'utilisateur

--- src/pages/montres/index.vue
+++ src/pages/montres/index.vue
@@ -6,7 +6,9 @@ import {user } from '@/supabase'
 <template>
     <div v-if="user">
         <h1 class="md:text-3xl text-xl xs:ml-12 sm:ml-24 md:mr-16 lg:mr-24 mt-3 ml-5 font-lexend font-bold">Vos montres enregistrées</h1>
-        <listMontre class="flex flex-wrap gap-2" />
+        <div class="my-8">
+            <listMontre class="flex flex-wrap gap-2" />
+        </div>
     </div>
     <div v-else>
         <h1>Connectez-vous pour voir vos montres !</h1>
