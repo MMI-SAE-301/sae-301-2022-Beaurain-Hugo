6370bd176d41c9608e2b327346fe540e2f344fe9
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-10-27 13:13:19 +0200

    Composants pour l'affichage des materiaux du boitier et du bracelet

---
+++ src/components/listMateriauxBoi.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import { materiauxBoi } from "@/types";
+
+</script>
+
+<template>
+    
+
+<FormKit
+    
+value="#FFFFFF"
+type="radio"
+:options="materiauxBoi"
+:sections-schema="{inner: {$el:null}, decorator:{$el:null}}"
+input-class="peer sr-only" 
+options-class="flex gap-1"
+>
+<template #label="context">
+                <div class="h-6 w-6 rounded-full border-2 peer-checked:border-red-600" :style="{backgroundImage: `url('${context.option.value}')`}"></div>
+                <span class="sr-only">{{context.option.label}}</span>
+            </template>
+</FormKit>
+</template>
